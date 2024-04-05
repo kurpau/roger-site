ed9175a3310bb67b2b88a543561d903556a03254
kurpau <paul.kurlavicius@gmail.com>
2024-04-05 14:54:04 +0300

    Add component lib

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NavbarTop />
 </template>
+
+<script lang="ts" setup>
+import NavbarTop from "/src/components/NavbarTop.vue";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
-    '/': { prerender: true },
+    "/": { prerender: true },
   },
+  modules: ["@nuxtjs/tailwindcss"],
 });
