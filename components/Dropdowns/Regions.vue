6308b5ec007cec7dfefb37aba1cc098746716ad4
kurpau <paul.kurlavicius@gmail.com>
2024-04-17 17:34:18 +0300

    Adjust dropdown css

--- components/Dropdowns/Regions.vue
+++ components/Dropdowns/Regions.vue
@@ -2,20 +2,19 @@
   <div class="my-auto">
     <SfDropdown v-model="isOpen">
       <template #trigger>
-        <SfButton variant="secondary" size="sm" @click="toggle()">
+        <SfButton class="min-w-12" variant="secondary" size="sm" @click="toggle()">
           <div v-if="loading">Loading...</div>
-          <span v-else>{{ regionStore.country?.display_name }} |
-            {{ regionStore.region?.currency_code?.toUpperCase() }}</span>
+          <span v-else>{{ regionStore.country?.iso_3.toUpperCase() }} </span>
         </SfButton>
       </template>
-      <div v-for="region in regionStore.regions" :key="region.id">
+      <div class="relative z-10" v-for="region in regionStore.regions" :key="region.id">
         <ul class="p-2 bg-gray-100 rounded">
+          <li class="font-bold">{{ region.name }}</li>
           <li v-for="country in region.countries" :key="country.id">
             <button @click="
               regionStore.updateRegion({ region: region, country: country })
               ">
-              {{ country.display_name }} \
-              {{ region.currency_code.toUpperCase() }}
+              {{ country.display_name }}
             </button>
           </li>
         </ul>
